f7e713ec5ba9ba037b5d6fc3e366d97349f79727
Dominic Mercier <dominic@dominic-mercier.com>
2016-03-03 16:39:57 -0500

    integration Issue Details

--- erudit/static/js/build/modernizr-dev.js
+++ erudit/static/js/build/modernizr-dev.js
@@ -1381,6 +1381,27 @@ Detects support for SVG in `<embed>` or `<object>` elements.
   
 /*!
 {
+  "name": "input[search] search event",
+  "property": "search",
+  "tags": ["input","search"],
+  "authors": ["Calvin Webster"],
+  "notes": [{
+    "name": "Wufoo demo",
+    "href": "https://www.wufoo.com/html5/types/5-search.html?"
+  }, {
+    "name": "CSS Tricks",
+    "href": "https://css-tricks.com/webkit-html5-search-inputs/"
+  }]
+}
+!*/
+/* DOC
+There is a custom `search` event implemented in webkit browsers when using an `input[search]` element.
+*/
+
+  Modernizr.addTest('inputsearchevent',  hasEvent('search'));
+
+/*!
+{
   "name": "DOM Pointer Events API",
   "property": "pointerevents",
   "tags": ["input"],
@@ -1567,27 +1588,6 @@ This test will also return `true` for Firefox 4 Multitouch support.
     }
     return bool;
   });
-
-/*!
-{
-  "name": "input[search] search event",
-  "property": "search",
-  "tags": ["input","search"],
-  "authors": ["Calvin Webster"],
-  "notes": [{
-    "name": "Wufoo demo",
-    "href": "https://www.wufoo.com/html5/types/5-search.html?"
-  }, {
-    "name": "CSS Tricks",
-    "href": "https://css-tricks.com/webkit-html5-search-inputs/"
-  }]
-}
-!*/
-/* DOC
-There is a custom `search` event implemented in webkit browsers when using an `input[search]` element.
-*/
-
-  Modernizr.addTest('inputsearchevent',  hasEvent('search'));
 
 
   // Run each test
